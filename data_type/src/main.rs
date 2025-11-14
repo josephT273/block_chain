500c19460fd2deb1f31b7a33db6431b793bf0bc4
Joseph Tadesse <josephtadessem@gmail.com>
2025-11-14 11:53:24 +0300

    multiplication opration on float and integer datatype

---
+++ data_type/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("Sum of 5 + 10 = {sum}");
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("Difference of 95.5 - 4.3 = {difference}");
+    // multiplication
+    let product = 4 * 30;
+    println!("Multiplication of 4 * 30 = {product}");
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("Division of 56.7 / 32.2 = {quotient}");
+    let truncated = -5 / 3; // Results in -1
+    println!("Trancuated devision of -5 / 3 = {truncated}");
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("Remainder of 43 / 5 = {remainder}");
+}
