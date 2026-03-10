99a447019147f19dfaa8a70665ba73054103eb07
josepht273 <josephtadessem@gmail.com>
2026-03-10 16:50:30 +0300

    rust happy path

--- rust_enum/src/main.rs
+++ rust_enum/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 struct Ipv4Addr {}
 
 struct Ipv6Addr {}
@@ -11,6 +13,27 @@ enum IpAddr {
 enum UsState {
     Alabama,
     Alaska,
+}
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1956,
+        }
+    }
+}
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relatively new"))
+    }
 }
 
 enum Coin {
@@ -66,4 +89,19 @@ fn main() {
     println!("{five:?}");
     println!("{none:?}");
     value_in_cent(Coin::Quarter(UsState::Alaska));
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximux is configured to be {max}");
+    }
+
+    let coin = Coin::Penny;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quater from {state:?}!");
+    } else {
+        count += 1;
+    }
+
+    println!("Coin {count}");
 }
