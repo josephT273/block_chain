9ae5fa19f82ad64265159a7e89b1b5a04cfb73ad
josepht273 <josephtadessem@gmail.com>
2026-03-10 15:03:36 +0300

    enums in rust

---
+++ rust_enum/src/main.rs
@@ -0,0 +1,25 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("Home: {home} loopback: {loopback}");
+}
