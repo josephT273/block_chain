6f0c6d2130b31a6df1815addb828b5ea5662862f
josepht273 <josephtadessem@gmail.com>
2026-03-10 15:58:57 +0300

    catch all paters and the _Placeholder

--- rust_enum/src/main.rs
+++ rust_enum/src/main.rs
@@ -32,10 +32,38 @@ fn value_in_cent(coin: Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
+    let dice_roll = 9;
+
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(), // other => move_player(other),
+                       // _ => () this also helps to include all numbers
+    }
+
+    fn reroll() {}
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+
     let some_number = Some(5);
     let some_char = Some('e');
     let absent_number: Option<i32> = None;
 
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{six:?}");
+    println!("{five:?}");
+    println!("{none:?}");
     value_in_cent(Coin::Quarter(UsState::Alaska));
 }
