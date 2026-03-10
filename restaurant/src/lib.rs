763e9a1730f05a850e2a127859d7d3c2f8701dc1
josepht273 <josephtadessem@gmail.com>
2026-03-10 23:21:22 +0300

    setup crate in diffrent files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,5 +1,3 @@
-fn deliver_order() {}
-
 mod back_of_house {
     pub struct Breakfast {
         pub toast: String,
@@ -19,29 +17,9 @@ mod back_of_house {
             }
         }
     }
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 pub use crate::front_of_house::hosting;
 
